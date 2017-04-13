57e80d2bae91c57bf8c1f05d93ca71fdd4b85e03
khanhbui1412240 <idol.brucelee1996@gmail.com>
2017-04-13 21:00:13 +0700

    no message

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
+var exphbs  = require('express-handlebars');
 app.use(express.static('public'));
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+
+app.engine('hbs', exphbs({}));
+app.set('view engine', 'hbs');
+
+app.get('/hbs', function (req, res) {
+  res.render('index',{title:'Homepage',message:'This is my Homepage'})
 });
 port = 3000;
 app.listen(3000, function () {
